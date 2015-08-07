38d2cdd4f3a188205f6f4fc7ed87cce644e998cd
Trezy <trezy.peebles@inin.com>
2015-08-07 12:45:07 -0500

    Add grunt-bump

---
+++ grunt/bump.js
@@ -0,0 +1,14 @@
+module.exports = {
+  options: {
+    commitFiles: [
+      'package.json',
+      'bower.json'
+    ],
+    files: [
+      'package.json',
+      'bower.json'
+    ],
+    pushTo: 'origin',
+    syncVersions: true
+  }
+}
