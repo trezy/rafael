af0244d1a44f26146e42b71b5495b4a2a53befda
Trezy <t@trezy.com>
2022-04-11 10:29:36 -0500

    upgrade to modern ESM

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Rafael } from './Rafael.js'
+import { Task } from './Task.js'
+
+export default Rafael
+export {
+	Rafael,
+	Task,
+}
--- src/rAFael.js
+++ src/rAFael.js
@@ -1,235 +1,226 @@
-/*
-  # rAFael
+// Local imports
+import { Task } from './Task.js'
 
-  */
- class rAFael {
-  /*
-    The `rAFael.paused` flag determines if this entire rAFael instance should be paused.
-  */
-  this.paused = false
 
-  /*
-    `rAFael.tasks` maintains a record of all currently running tasks
-    associated with this instance of rAFael.
-  */
-  this.tasks = {}
 
 
 
+/**
+ * The main Rafael class.
+ */
+export class Rafael {
+	/****************************************************************************\
+	 * Private instance properties
+	\****************************************************************************/
 
+	#frame = 0
 
-  /*
-    ## `new rAFael`
+	#isPaused = false
 
-    Creating a new `rAFael` instance will kick off its loop and start to
-    running tasks.
-  */
-  constructor () {
-    this._startLoop()
-  }
+	#tasks = {}
 
+	/**
+	 * Starts the `requestAnimationFrame` loop.
+	 */
+	#loop = () => {
+		if (!this.#isPaused) {
+			const taskKeys = Object.values(this.#tasks)
 
+			taskKeys.forEach(task => {
+				const {
+					framerate,
+					isPaused,
+				} = task
 
+				if ((framerate === 60 || this.#shouldRunTask(task)) && !isPaused) {
+					this.#taskCaller(task)
+				}
+			})
 
+			this.#frame += 1
+		}
 
-  /*
-    ## `rAFael.schedule(taskId, task, options)`
+		window.requestAnimationFrame(this.#loop)
+	}
 
-    Schedule a task to be run in our loop. It takes up to three arguments:
-    `taskId`, `task`, and `options`.
 
-    * `taskId` (optional) represents the ID you'll use to interact with this task
-    later on. If an ID isn't set then it will be the index of the new task in
-    `this.tasks`.
 
-    * `task` (required) is the function to be run on this `rAFael`.
 
-    * `options` (optional) is a hash of options thingies?
 
-      * `context` (optional) is the value of `this` within the function. If not
-      set, the context will be `window`.
+	/****************************************************************************\
+	 * Constructor
+	\****************************************************************************/
 
-      * `paused` (optional) determines whether this task will run immediately
-      after being added to the schedule
-
-      * `framerate` (optional) is the speed at which the task should be run in
-      frames per second (fps)
-  */
-  schedule (id, task, options) {
-    let framerate = null
-
-    if (typeof id !== 'string') {
-      options = task
-      task = id
-      id = Object.keys(this.tasks).length
-    }
-
-    options || (options = {})
-
-    this._debug = options.debug || false
-    this._frame = 0
-    framerate = options.framerate || 60
-
-    if (this._debug) {
-      console.log('Scheduling task', id)
-    }
-
-    if (framerate > 60) {
-      throw new RangeError('Framerate may not be higher than 60; Requested framerate is', framerate)
-      return
-    }
-
-    if (this.tasks[id]) {
-      throw new RangeError('A task with the ID "' + id + '" already exists')
-      return
-    }
-
-    this.tasks[id] = {
-      context: options.context || window,
-      paused: options.paused || false,
-      framerate: framerate,
-      task: task
-    }
-
-    return id
-  }
-
-
-
-
-
-  /*
-    ## `rAFael.unschedule(id)`
-
-    Remove a task from our loop. `id` is ID of the task to be removed from this `rAFael`.
-  */
-  unschedule = function schedule (id) {
-    if (this._debug) {
-      console.log('Unscheduling task', id)
-    }
-
-    delete this.tasks[id]
-    return !this.tasks[id]
-  }
-
-
-
-
-
-  /*
-    ## `rAFael.clear()`
-
-    Remove all tasks from the `rAFael`. We just overwrite the original array
-    since this is a destructive operation.
-  */
-  clear () {
-    if (this._debug) {
-      console.log('Clearing tasks')
-    }
-
-    this.tasks = {}
-  }
-
-
-
-
-
-  /*
-    ## `rAFael.pause(id)`
-
-    TODO: Describe
-  */
-  pause (id) {
-    if (this._debug) {
-      console.log('Pausing', id || 'all tasks')
-    }
-
-    if (!id) {
-      this.paused = true
-    } else if (this.tasks[id]) {
-      this.tasks[id].paused = true
-    }
-  }
-
-
-
-
-
-  /*
-    ## `rAFael.start(id)`
-
-    TODO: Describe
-  */
-  start (id) {
-    if (this._debug) {
-      console.log('Starting', id || 'all tasks')
-    }
-
-    if (id) {
-      this.tasks[id].paused = false
-    } else {
-      this.paused = false
-    }
-  }
-
-
-
-
-
-  /*
-    ## `rAFael._taskCaller(taskObject)`
-
-    Start the loop `requestAnimationFrame` loop for this `rAFael`.
-  */
-  _taskCaller (taskObject) {
-    taskObject.task.call(taskObject.context)
-  }
-
-
-
-
-
-  /*
-    ## `rAFael._startLoop()`
-
-    Start the loop `requestAnimationFrame` loop for this `rAFael`.
-  */
-  _startLoop = () => {
-    const schedule = this
-
-    if (!this.paused) {
-      for (const [taskName, task] of Object.entries(this.tasks)) {
-        const task = this.tasks[tasks[i]]
-
-        if ((task['framerate'] === 60 || this._shouldRun(task['framerate'])) && !task.paused) {
-          this._taskCaller(task)
-        }
-      }
-
-      this._frame++
-    }
-
-    window.requestAnimationFrame(schedule._startLoop)
-  }
-
-
-
-
-
-  /*
-    ## `rAFael._shouldRun()`
-
-    Compares the passed in framerate against the current frame to determine if the
-    task should run on this frame
-  */
-  _shouldRun (framerate) {
-    return !(Math.floor(this._frame % (60 / framerate)))
-  }
+	/**
+	 * Creates a new Rafael instance.
+	 *
+	 * @param {object} [options] All options.
+	 * @param {boolean} [options.startImmediately = true] Whether or not to start the `requestAnimationFrame` loop immediately upon creation of this instance.
+	 */
+	constructor(options = {}) {
+		const {
+			startImmediately = true,
+		} = options
+
+		if (startImmediately) {
+			this.#loop()
+		}
+	}
+
+
+
+
+
+	/****************************************************************************\
+	 * Private instance methods
+	\****************************************************************************/
+
+	/**
+	 * Determines if a task should be run based on its framerate.
+	 *
+	 * @param {Task} task The task to be checked.
+	 * @returns {boolean} Whether or not the task should be run.
+	 */
+	#shouldRunTask(task) {
+		const { framerate } = task
+
+		return !(Math.floor(this.#frame % (60 / framerate)))
+	}
+
+	/**
+	 * Runs a task with the appropriate context
+	 *
+	 * @param {Task} config Configuration for the task being called.
+	 */
+	#taskCaller(config) {
+		const {
+			context,
+			task,
+		} = config
+
+		task.call(context)
+	}
+
+
+
+
+
+	/****************************************************************************\
+	 * Public instance methods
+	\****************************************************************************/
+
+	/**
+	 * Removes all tasks. We just overwrite the original dictionary since this is a destructive operation.
+	 */
+	clear() {
+		this.#tasks = {}
+	}
+
+	/**
+	 * Pauses this instance. Can also pause an individual task.
+	 *
+	 * @param {string} [taskID] The ID of the task to pause.
+	 */
+	pause(taskID) {
+		// if (this._debug) {
+		// 	console.log('Pausing', taskID || 'all tasks')
+		// }
+
+		if (!taskID) {
+			this.#isPaused = true
+		} else if (this.tasks[taskID]) {
+			this.tasks[taskID].pause()
+		}
+	}
+
+	/**
+	 * Starts this instance. Can also start an individual task.
+	 *
+	 * @param {string} [taskID] The ID of the task to start.
+	 */
+	start(taskID) {
+		// if (this._debug) {
+		// 	console.log('Starting', id || 'all tasks')
+		// }
+
+		if (taskID) {
+			this.tasks[taskID].start()
+		} else {
+			this.#isPaused = false
+		}
+	}
+
+	/**
+	 * Adds a task to the schedule, ensuring it is run on the `requestAnimationFrame` loop.
+	 *
+	 * @param {Function} task The function to be run.
+	 * @param {object} [options] All options.
+	 * @param {*} [options.context = window] The context within which the function will run. If not set, the context will be `window`.
+	 * @param {number} [options.framerate = 60] The speed at which the task should be run in frames per second (fps).
+	 * @param {boolean} [options.isPaused = false] Whether this task should be running.
+	 * @param {string} [options.id] The ID you'll use to interact with this task later on. If an ID isn't set then it will be the index of the new task in `this.tasks`.
+	 * @returns {string} The ID of the created task.
+	 */
+	schedule(task, options = {}) {
+		const {
+			context = window,
+			framerate = 60,
+			isPaused = false,
+			id: taskID = String(Object.keys(this.tasks).length),
+		} = options
+
+		if (framerate > 60) {
+			throw new RangeError(`Framerate may not be higher than 60; requested framerate is ${framerate}.`)
+		}
+
+		if (this.tasks[taskID]) {
+			throw new RangeError(`A task with the ID \`${taskID}\` already exists.`)
+		}
+
+		this.#tasks[taskID] = new Task({
+			context,
+			isPaused,
+			framerate,
+			task,
+		})
+
+		return taskID
+	}
+
+	/**
+	 * Removes a task from the schedule.
+	 *
+	 * @param {string} taskID The ID of the task to be removed from the schedule.
+	 * @returns {boolean} Whether or not we successfully removed the task from the schedule.
+	 */
+	unschedule(taskID) {
+		// if (this._debug) {
+		// 	console.log('Unscheduling task', id)
+		// }
+
+		delete this.#tasks[taskID]
+		return !this.#tasks[taskID]
+	}
+
+
+
+
+
+	/****************************************************************************\
+	 * Public instance getters/setters
+	\****************************************************************************/
+
+	/**
+	 * @returns {object} Whether or not this instance is currently running.
+	 */
+	get isPaused() {
+		return this.#isPaused
+	}
+
+	/**
+	 * @returns {object} A dictionary of the current tasks.
+	 */
+	get tasks() {
+		return { ...this.#tasks }
+	}
 }
-
-
-
-
-
-export default rAFael
-export { rAFael }
