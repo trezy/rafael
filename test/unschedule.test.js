c7776058fe1572c2e983fc97ea416c6d25c41675
Trezy <t@trezy.com>
2022-04-13 13:12:50 -0500

    test(unschedule): update to use fake timers

--- test/unschedule.test.js
+++ test/unschedule.test.js
@@ -28,10 +28,14 @@ describe('unschedule', function() {
 		})
 
 		// @ts-ignore
+		this.clock = sinon.useFakeTimers({ global: window })
+
+		// @ts-ignore
 		globalThis.window = window
 	})
 
 	after(function() {
+		this.clock.restore()
 		globalThis.window.close()
 	})
 
@@ -47,27 +51,32 @@ describe('unschedule', function() {
 		schedule(sinon.fake(), { id: 'foo' })
 
 		// eslint-disable-next-line no-unused-expressions
-		expect(Object.keys(state.tasks)).to.have.lengthOf(1)
+		expect(state.tasks).to.be.an('object')
+			.with.all.keys('foo')
 
 		unschedule('foo')
 
 		// eslint-disable-next-line no-unused-expressions
-		expect(state.tasks).to.be.an('object').that.is.empty
+		expect(state.tasks).to.be.an('object')
+			.that.is.empty
 	})
 
 	it('should stop the task runner if there are no tasks left', function() {
 		schedule(sinon.fake(), { id: 'foo' })
 		schedule(sinon.fake(), { id: 'bar' })
-		schedule(sinon.fake(), { id: 'baz' })
 
 		// eslint-disable-next-line no-unused-expressions
 		expect(state.loopID).to.not.be.null
+		expect(state.tasks).to.be.an('object')
+			.with.keys('foo', 'bar')
 
 		unschedule('foo')
 		unschedule('bar')
-		unschedule('baz')
 
 		// eslint-disable-next-line no-unused-expressions
 		expect(state.loopID).to.be.null
+		// eslint-disable-next-line no-unused-expressions
+		expect(state.tasks).to.be.an('object')
+			.that.is.empty
 	})
 })
